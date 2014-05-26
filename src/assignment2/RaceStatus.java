aaac5f9df125390c00abbe7beda20d2259e30c37
Rushabh Shethia <forcelyopen@gmail.com>
2014-05-26 15:08:45 +1000

    Update RaceStatus.java

--- src/assignment2/RaceStatus.java
+++ src/assignment2/RaceStatus.java
@@ -2,9 +2,13 @@
 package assignment2;
 
 /**
- *
- * @author rsshethia
+ * Assignment 2 - CSE1IOO Semester 1, 2014.
+ * @author Orien, Dean and Rushabh
+ * @version 1.0
+ * 
+ * RaceStatus is enumerated class defining the status of contestant in a race.
  */
+ 
 public enum RaceStatus {
     
     NOT_STARTED, RACING, FINISHED;
